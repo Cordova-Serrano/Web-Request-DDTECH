0a2d4d6b214b60d3b51b406ec9ad6e09dc4a08d1
Cordova Serrano Eduardo Javier <60123447+svmpler@users.noreply.github.com>
2020-12-18 20:01:28 -0600

    version 1

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WebRequest_DDTECH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listCategories = new System.Windows.Forms.ListBox();
+            this.categorias = new System.Windows.Forms.Button();
+            this.labelCategoria = new System.Windows.Forms.Label();
+            this.listSubCat = new System.Windows.Forms.ListBox();
+            this.labelSubCat = new System.Windows.Forms.Label();
+            this.listProductos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listCategories
+            // 
+            this.listCategories.Font = new System.Drawing.Font("Bebas Neue", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listCategories.FormattingEnabled = true;
+            this.listCategories.ItemHeight = 23;
+            this.listCategories.Location = new System.Drawing.Point(13, 77);
+            this.listCategories.Name = "listCategories";
+            this.listCategories.Size = new System.Drawing.Size(120, 878);
+            this.listCategories.TabIndex = 0;
+            this.listCategories.SelectedIndexChanged += new System.EventHandler(this.listCategories_SelectedIndexChanged);
+            // 
+            // categorias
+            // 
+            this.categorias.Font = new System.Drawing.Font("Bebas Neue", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.categorias.Location = new System.Drawing.Point(12, 12);
+            this.categorias.Name = "categorias";
+            this.categorias.Size = new System.Drawing.Size(120, 49);
+            this.categorias.TabIndex = 1;
+            this.categorias.Text = "C A T E G O R I A S";
+            this.categorias.UseVisualStyleBackColor = true;
+            this.categorias.MouseClick += new System.Windows.Forms.MouseEventHandler(this.categorias_MouseClick);
+            // 
+            // labelCategoria
+            // 
+            this.labelCategoria.AutoSize = true;
+            this.labelCategoria.BackColor = System.Drawing.Color.LightSlateGray;
+            this.labelCategoria.Font = new System.Drawing.Font("Bebas Neue", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCategoria.Location = new System.Drawing.Point(163, 31);
+            this.labelCategoria.Name = "labelCategoria";
+            this.labelCategoria.Size = new System.Drawing.Size(58, 25);
+            this.labelCategoria.TabIndex = 2;
+            this.labelCategoria.Text = "label1";
+            this.labelCategoria.Visible = false;
+            // 
+            // listSubCat
+            // 
+            this.listSubCat.Font = new System.Drawing.Font("Bebas Neue", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listSubCat.FormattingEnabled = true;
+            this.listSubCat.ItemHeight = 23;
+            this.listSubCat.Location = new System.Drawing.Point(159, 77);
+            this.listSubCat.Name = "listSubCat";
+            this.listSubCat.Size = new System.Drawing.Size(218, 878);
+            this.listSubCat.TabIndex = 3;
+            this.listSubCat.SelectedIndexChanged += new System.EventHandler(this.listSubCat_SelectedIndexChanged);
+            // 
+            // labelSubCat
+            // 
+            this.labelSubCat.AutoSize = true;
+            this.labelSubCat.BackColor = System.Drawing.Color.LightSlateGray;
+            this.labelSubCat.Font = new System.Drawing.Font("Bebas Neue", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelSubCat.Location = new System.Drawing.Point(406, 31);
+            this.labelSubCat.Name = "labelSubCat";
+            this.labelSubCat.Size = new System.Drawing.Size(58, 25);
+            this.labelSubCat.TabIndex = 5;
+            this.labelSubCat.Text = "label1";
+            this.labelSubCat.Visible = false;
+            // 
+            // listProductos
+            // 
+            this.listProductos.Font = new System.Drawing.Font("Bebas Neue", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listProductos.FormattingEnabled = true;
+            this.listProductos.ItemHeight = 23;
+            this.listProductos.Location = new System.Drawing.Point(411, 77);
+            this.listProductos.Name = "listProductos";
+            this.listProductos.Size = new System.Drawing.Size(1470, 878);
+            this.listProductos.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.RoyalBlue;
+            this.ClientSize = new System.Drawing.Size(1904, 961);
+            this.Controls.Add(this.listProductos);
+            this.Controls.Add(this.labelSubCat);
+            this.Controls.Add(this.listSubCat);
+            this.Controls.Add(this.labelCategoria);
+            this.Controls.Add(this.categorias);
+            this.Controls.Add(this.listCategories);
+            this.Name = "Form1";
+            this.Text = "WebRequest - DDTECH";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listCategories;
+        private System.Windows.Forms.Button categorias;
+        private System.Windows.Forms.Label labelCategoria;
+        private System.Windows.Forms.ListBox listSubCat;
+        private System.Windows.Forms.Label labelSubCat;
+        private System.Windows.Forms.ListBox listProductos;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace WebRequest_DDTECH
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void categorias_MouseClick(object sender, MouseEventArgs e)
+        {
+            string urlMain = "https://ddtech.mx";
+
+            string responseFromServer = responseWebRequest(urlMain);
+
+            int f;
+            int g;
+            int h;
+            int k = 0;
+
+            for (int i = 0; i < 15; i++)
+            {
+                f = responseFromServer.IndexOf("ddtech-icon icon-category", k);
+
+                g = responseFromServer.IndexOf("</i>", f);
+
+                h = responseFromServer.IndexOf("</a>", g + 1);
+
+                string cadena = responseFromServer.Substring(g + 4, h - g - 4);
+
+                listCategories.Items.Add(cadena); //Se añade la categoría a la listBox
+
+                k = responseFromServer.IndexOf("ddtech-icon icon-category", h);
+            }
+        }
+        private void listCategories_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listProductos.Items.Clear(); //se limpia el arreglo de Productos para ser reemplazado por una nueva seleccion 
+            listSubCat.Items.Clear(); //se limpia el arreglo de Sub Categorias para ser reemplazado por una nueva seleccion 
+            labelCategoria.Text = (string)listCategories.Items[listCategories.SelectedIndex];
+            labelCategoria.Visible = true;
+            CategoryItems((string)listCategories.Items[listCategories.SelectedIndex]);
+        }
+        private void CategoryItems(string category)
+        {
+            string urlCat = "https://ddtech.mx";
+
+            //Aqui se recibe toda la informacion de la url 
+            string responseFromServer = responseWebRequest(urlCat);
+
+            //Variables para las Categorías
+            int f;
+            int g;
+            int h;
+            int k = 0;
+            //Variables para las Sub - Categorías
+            int q;
+            int w;
+            int e;
+            int r;
+
+            for (int i = 0; i < 15; i++)
+            {
+                f = responseFromServer.IndexOf("ddtech-icon icon-category", k);
+
+                g = responseFromServer.IndexOf("</i>", f);
+
+                h = responseFromServer.IndexOf("</a>", g + 1);
+
+                string cadena = responseFromServer.Substring(g + 4, h - g - 4);
+
+                if (category == cadena)//Se encontro la categoría que se buscaba
+                {
+                    //Hora de obtener sus Sub - Categorías
+                    //Para el ciclo debemos buscar algun patron que nos determine el fin de las SubCategorias
+                    //dropdown menu-item esta cada vez que se inicia una nueva categoria
+                    r = h;
+                    bool flag = true;
+
+                    while (flag)
+                    {
+                        q = responseFromServer.IndexOf("title=", r);
+
+                        w = responseFromServer.IndexOf("href=", q);
+
+                        //Aqui encontramos la Sub Categoría
+                        string subCadena = responseFromServer.Substring(q + 7, w - q - 9);
+                        listSubCat.Items.Add(subCadena); //Se añade la categoría a la listBox
+
+                        r = responseFromServer.IndexOf("title=", w); //Aqui encuentra otro title =
+                        //Si despues de w se encuentra un dropdown menu-item significa que termino la categoría
+                        e = responseFromServer.IndexOf("dropdown menu-item", w); //Aqui ya se tiene la posicion de nuestro final = 12462
+
+                        if (r > e)
+                        {
+                            flag = false;
+                        }
+                    }
+                }
+                k = responseFromServer.IndexOf("ddtech-icon icon-category", h);
+            }
+        }
+        private void listSubCat_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listProductos.Items.Clear(); //se limpia el arreglo de Productos para ser reemplazado por una nueva seleccion 
+            labelSubCat.Text = (string)listSubCat.Items[listSubCat.SelectedIndex];
+            labelSubCat.Visible = true;
+            SubCategoryItems((string)listSubCat.Items[listSubCat.SelectedIndex]);
+        }
+        private void SubCategoryItems(string Subcategory) //Recibe el nombre de la sub categoria seleccionada 
+        {
+            //Nombre de la Categoria Padre
+            string categoria = labelCategoria.Text;
+            //Nombre de la Subcategoría deseada
+            string subCategoria = labelSubCat.Text;
+
+            string url;
+
+            bool flagPaginas = true;
+            int x = 1; //Se inicia en la página 1 
+
+            while (flagPaginas)
+            {
+                //Variables para las Categorías
+                int f = 0;
+                int g;
+                int h;
+                int k = 0;
+
+                if (labelSubCat.Text == "Ver todo") //Si se selecciona Ver todo 
+                {
+                    url = "https://ddtech.mx/productos/" + categoria + "?pagina=" + x;
+                    Console.WriteLine(url); //url visitada 
+                }
+                else
+                {
+                    url = "https://ddtech.mx/productos/" + categoria + "/" + subCategoria + "?pagina=" + x;
+                    Console.WriteLine(url); //url visitada 
+                }
+                //Hora de encontrar la informacion de esta Sub Categoría 
+                string responseFromServer = responseWebRequest(url);
+
+                //Obtenemos la cantidad de productos en la página
+                bool flag = true;
+                string cantidad = " ";
+                int contProductos = 0;
+
+                int end = responseFromServer.IndexOf("<div class=\"product-pagination\">", k); // Posicion del fin de los productos
+
+                while (flag)
+                {
+                    f = responseFromServer.IndexOf("<div class=\"product-image\">", k); //Marca el inicio del produto
+
+                    if (f != -1) //Si la posicion de f es diferente a -1
+                    {
+                        cantidad = responseFromServer.Substring(f, 27);
+                    }
+
+                    if (cantidad == "<div class=\"product-image\">") //Se encontro la palabra
+                    {
+                        contProductos++;
+                    }
+
+                    if (f == -1) //Si la posicion de f es -1 se termino la busqueda
+                    {
+                        flag = false;
+                    }
+
+                    k = f + 27;
+                } //Con este While se obtienen la cantidad de productos en la página 
+
+                contProductos -= 1;
+
+                Console.WriteLine("Página : " + x + " " + categoria);
+                Console.WriteLine("Cantidad de Productos: " + contProductos);
+
+                for (int j = 0; j < contProductos; j++)
+                {
+                    f = responseFromServer.IndexOf("<div class=\"product-image\">", k);
+                    //Console.WriteLine("f :" + f);
+
+                    g = responseFromServer.IndexOf("title=", f);
+                    //Console.WriteLine("g :" + g);
+                    h = responseFromServer.IndexOf("href=", g);
+                    //Console.WriteLine("h :" + h);
+
+                    //Aqui encontramos el Nombre del Producto
+                    string nombre = responseFromServer.Substring(g + 7, h - g - 9); // Nombre del producto 
+                    Console.WriteLine("Producto : " + nombre);
+
+                    f = responseFromServer.IndexOf("class=\"price\">", h); //Dependiendo de la cantidad de veces que se encuentra esa palabra es la cantidad de productos 
+                                                                           //Console.WriteLine("f :" + f);
+                    g = responseFromServer.IndexOf("</span>", f);
+                    //Console.WriteLine("g :" + g);
+
+                    //Aqui encontramos el Precio del Producto
+                    string precio = responseFromServer.Substring(f + 14, g - f - 14); // Nombre del producto 
+                    Console.WriteLine("Precio : " + precio);
+
+                    //Aqui añadimos la info a la lista
+                    listProductos.Items.Add(nombre + " " + precio);
+                    k = g;
+                } //For para la impresion de los productos de la página actual 
+
+                //Detectamos si es la ultima página, cuando una página llega a su fin cuenta con este patron : href="javascript:return false;">Siguiente</a>
+                int endPage = 0;
+
+                endPage = responseFromServer.IndexOf("href=\"javascript:return false;\">Siguiente</a>", 0); //Se busca esta palabra en el código fuente
+
+                if (endPage != -1)
+                {
+                    flagPaginas = false;
+                }
+                x++;
+            }
+        }
+        public string responseWebRequest(string url)
+        {
+            // Create a request for the URL. 		
+            WebRequest request = WebRequest.Create(url);
+            // If required by the server, set the credentials.
+            request.Credentials = CredentialCache.DefaultCredentials;
+            // Get the response.
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            // Display the status.
+            //Console.WriteLine(response.StatusDescription); //Ok si encontro la info
+            // Get the stream containing content returned by the server.
+            Stream dataStream = response.GetResponseStream();
+            // Open the stream using a StreamReader for easy access.
+            StreamReader reader = new StreamReader(dataStream);
+            // Read the content.
+            string responseFromServer = reader.ReadToEnd(); //Aqui se obtiene toda la informacion
+
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            return responseFromServer;
+        }
+    }
+}
+
